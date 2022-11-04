1e43f3080a4e285125835691a08204b17041506d
iganeshk <kumar@iganesh.com>
2022-11-04 08:57:15 -0500

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,6 @@ cd /opt/adwireguard
 # run AdGuardHome in background
 echo "Running AdGuardHome in background"
 ./AdGuardHome --no-check-update -c "/opt/adwireguard/conf/AdGuardHome.yaml" -w "/opt/adwireguard/work" -h "0.0.0.0" &
-# wait until AdguardHome is up
-echo "Waiting for AdguardHome to open port 53..."
-while ! nc -z localhost 53; do   
-  sleep 1
-done
 
 # run wg-easy app using node
 echo "Running wg-easy in background"
